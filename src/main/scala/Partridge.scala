188f78dc80528674fc26dcb98a0686e485fc9038
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-02-19 14:36:19 +0000

    add test 2

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,5 +1,14 @@
 object Partridge {
 
-def apply(x: List[String]):String =
-    "Lynn, I've pierced my foot on a spike!!"
+
+  val terms: List[String] = "Partridge" :: "PearTree" :: "Chat" :: "Dan" :: "Toberlone" :: "Lynn" :: "AlphaPapa" :: "Nomad" :: Nil
+
+
+  def apply(x: List[String]): String =
+    if (x == List("AlphaPapa")) {
+      "Mines a pint!"
+
+    } else {
+      "Lynn, I've pierced my foot on a spike!!"
+    }
 }
--- src/test/scala/testPartridgeSpec.scala
+++ src/test/scala/testPartridgeSpec.scala
@@ -7,5 +7,8 @@ class testPartridgeSpec extends WordSpec with MustMatchers{
     "return 'Lynn, I've pierced my foot on a spike'!!'" in {
       Partridge(List.empty) mustEqual "Lynn, I've pierced my foot on a spike!!"
     }
+    "return 'Mines a pint'!" in {
+      Partridge(List("AlphaPapa")) mustEqual "Mines a pint!"
+    }
   }
 }
