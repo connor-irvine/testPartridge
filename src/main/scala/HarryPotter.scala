6cceb04df65f82e84519069b5973ba1dbf24cf4c
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-03-07 09:20:21 +0000

    add team solution to Harry Potter

--- src/main/scala/HarryPotter.scala
+++ src/main/scala/HarryPotter.scala
@@ -1,9 +1,28 @@
-object HarryPotter {
+package HarryPotter
 
 
-  def Potion(input: List[String]): String ={
+trait Potion
+
+case class PolyJuicePotion() extends Potion
+
+case class DraughtPotion() extends Potion
+
+case class Ingredient(name : String)
+
+
+
+object HarryPotter extends App {
+}
+
+object Cauldron {
+  def brew(ing: Ingredient*): Option[Potion] = {
+    val ingList = ing.toList
+    ingList match {
+
+      case List(Ingredient("Aconite"), Ingredient("Adder's Fork")) => Some(new PolyJuicePotion)
+      case List(Ingredient("Niffler's Fancy"), Ingredient("Nightshade")) => Some(new DraughtPotion)
+      case _ => None
+    }
 
   }
-
-
 }
